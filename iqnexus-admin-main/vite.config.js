b3d1e4b43b371f905afba29f954705b2fb8081ce
jayantasonowal <jayanta22755@gmail.com>
2025-11-12 19:51:35 +0530

    56

--- iqnexus-admin-main/vite.config.js
+++ iqnexus-admin-main/vite.config.js
@@ -5,4 +5,12 @@ export default defineConfig({
     tailwindcss(),
   ],
   base: "/",
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+    strictPort: true,
+    watch: {
+      usePolling: true,
+    },
+  },
 })
